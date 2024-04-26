264eafb0161978b6aa9b9c46cecbb7eec441e851
21862 <christopher.sinche@vtsd.com>
2024-04-26 11:57:37 -0400

    team 8

---
+++ 3.2.5/team8.py
@@ -0,0 +1,27 @@
+"""The function move(my_history, their_history) must return "r", "p", or "s".
+my_history and their_history are strings of the same length, perhaps length zero.
+"""
+import random
+
+strategy_name = "start with scissors and alternate"
+
+def move(my_history, their_history):
+    pick = random.choice(["r", "p", "s"])
+
+
+
+    # second move is always scissors
+    if (len(their_history) == 0):
+        return pick
+
+
+    if (len(their_history) == 1):
+        return "s"
+
+    else:
+        if their_history[-2] == "p":
+            return "s"
+        elif their_history[-2] == "r":
+            return "p"
+        else:
+            return "r"
